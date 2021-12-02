5a4642505927f8e10e1b31867f05246d385aa3e8
Jeff Lee <jeffjwl@gmail.com>
2021-12-01 21:12:32 -0600

    add pause/resume menu

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public bool isGameOver = false;
+    public bool isPaused = false;
     public Text gameOverText;
     public static GameManager singleton;
     public GameObject GameOverBg;
@@ -46,4 +47,38 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene("Main");
     }
+
+    public void Pause()
+    {
+        Setup();
+        singleton.gameOverText.text = "Game Paused";
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+    public void Resume()
+    {
+        singleton.GameOverBg.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    private void Update()
+    {
+        if(isGameOver)
+        {
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+
 }
